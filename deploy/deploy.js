25abda0b658047023344b4538f04350385294676
odd-amphora <odd_amphora@protonmail.com>
2021-10-26 18:43:18 -0400

    Add basic linting stuff

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -4,32 +4,34 @@
  * Example usage:
  *
  * pnpm hardhat deploy --network rinkeby
- * 
+ *
  * TODO: Find a solution for local chain.
  */
 
 const rinkebyJBDirectory = require(`@jbx-protocol/contracts/deployments/rinkeby/JBDirectory.json`);
 
 const getJBDirectoryAddress = (chainId) => {
-    if (chainId == 4) {
-        return rinkebyJBDirectory.address;        
-    }
+  if (chainId == 4) {
+    return rinkebyJBDirectory.address;
+  }
 
-    throw Error(`Chain ID ${chainId} is not yet supported.`);
-}
+  throw Error(`Chain ID ${chainId} is not yet supported.`);
+};
 
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
-    const { deploy } = deployments;
-    const { deployer } = await getNamedAccounts();
-    const chainId = await getChainId();
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
 
-    const jbDirectoryAddress = getJBDirectoryAddress(chainId);
-    console.log(`Deploying Juice NFT Market with Chain ID {${chainId}} and JBDirectory address ${jbDirectoryAddress}`);
+  const jbDirectoryAddress = getJBDirectoryAddress(chainId);
+  console.log(
+    `Deploying Juice NFT Market with Chain ID {${chainId}} and JBDirectory address ${jbDirectoryAddress}`,
+  );
 
-    await deploy('NFTMarket', {
-        from: deployer,
-        args: [jbDirectoryAddress],
-        log: true,
-        skipIfAlreadyDeployed: true,
-    });
+  await deploy('NFTMarket', {
+    from: deployer,
+    args: [jbDirectoryAddress],
+    log: true,
+    skipIfAlreadyDeployed: true,
+  });
 };
