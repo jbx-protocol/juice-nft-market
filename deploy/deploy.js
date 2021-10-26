fca305ec88e2b3a7fd086d09a775ec9d4ce4554f
Nicholas <nnnnicholas@users.noreply.github.com>
2021-10-26 16:55:30 -0400

    updated deploy script to match jb v2

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,18 +1,111 @@
-// const { ethers } = require("ethers"); // Not necessary because vscode has a built-in version
-
-async function main() {
+/**
+ * Deploys the Juice V2 contracts.
+ *
+ * Example usage:
+ *
+ * npx hardhat deploy --network rinkeby
+ */
+module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
-    const NFTMKT = await ethers.getContractFactory("NFTMKT");
-    const nftmkt = await NFTMKT.deploy('0x0000000000000000000000000000000000000000');
-
-    console.log('NFTMKT deployed to: ', nftmkt.address);
-}
-
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
+    const JBOperatorStore = await deploy('JBOperatorStore', {
+        from: deployer,
+        args: [],
+        log: true,
+        skipIfAlreadyDeployed: true,
     });
+
+    const JBPrices = await deploy('JBPrices', {
+        from: deployer,
+        args: [],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
+
+    const JBProjects = await deploy('JBProjects', {
+        from: deployer,
+        args: [JBOperatorStore.address],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
+
+    const JBDirectory = await deploy('JBDirectory', {
+        from: deployer,
+        args: [JBOperatorStore.address, JBProjects.address],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
+
+    const JBFundingCycleStore = await deploy('JBFundingCycleStore', {
+        from: deployer,
+        args: [JBDirectory.address],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
+
+    const JBTokenStore = await deploy('JBTokenStore', {
+        from: deployer,
+        args: [JBOperatorStore.address, JBProjects.address, JBDirectory.address],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
+
+    const JBSplitStore = await deploy('JBSplitsStore', {
+        from: deployer,
+        args: [JBOperatorStore.address, JBProjects.address, JBDirectory.address],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
+
+    const JBControllerV1 = await deploy('JBController', {
+        from: deployer,
+        args: [
+            JBOperatorStore.address,
+            JBProjects.address,
+            JBDirectory.address,
+            JBFundingCycleStore.address,
+            JBTokenStore.address,
+            JBSplitStore.address,
+        ],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
+
+    const JBETHPaymentTerminalStore = await deploy('JBETHPaymentTerminalStore', {
+        from: deployer,
+        args: [
+            JBPrices.address,
+            JBProjects.address,
+            JBDirectory.address,
+            JBFundingCycleStore.address,
+            JBTokenStore.address,
+        ],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
+
+    const _ = await deploy('JBETHPaymentTerminal', {
+        from: deployer,
+        args: [
+            JBOperatorStore.address,
+            JBProjects.address,
+            JBDirectory.address,
+            JBSplitStore.address,
+            JBETHPaymentTerminalStore.address,
+        ],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
+
+    const NFTMarket = await deploy('NFTMarket', {
+        from: deployer,
+        args: [
+            JBDirectory.address,
+        ],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
+    
+    // transfer ownership of terminalStore to terminal.
+};
