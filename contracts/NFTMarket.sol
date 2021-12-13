cee5a135e03876f16b481abebb950e3326252004
nicholas <nnnnicholas@users.noreply.github.com>
2021-12-13 16:13:28 -0500

    Removed erroneous transfer and added ReentrancyGuard

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -23,7 +23,7 @@ struct SaleRecipient {
  * @notice An NFT marketplace built for Juicebox projects.
  * @dev Compatible with ERC-721 NFTs only.
  */
-contract NFTMarket is IERC721Receiver {
+contract NFTMarket is IERC721Receiver, ReentrancyGuard {
     /**
     @dev The direct deposit terminals.
     */
@@ -40,7 +40,6 @@ contract NFTMarket is IERC721Receiver {
     //     // check that it sums to 10000
     //     saleRecipients[hash] = _recipients;
     // }
-    
 
     /**
      *  @notice Stores benificiaries of sale revenues and tokens
@@ -49,8 +48,7 @@ contract NFTMarket is IERC721Receiver {
      *  uint256 NFT tokenId
      *  SaleRecipient[] Array of split receipients.
      */
-    mapping(address => mapping(IERC721 => mapping(uint256 => SaleRecipient[])))
-        public recipientsOf;
+    mapping(address => mapping(IERC721 => mapping(uint256 => SaleRecipient[]))) public recipientsOf;
 
     /**
      * @notice Stores each NFT's price
@@ -58,8 +56,8 @@ contract NFTMarket is IERC721Receiver {
      * uint256 NFT tokenId
      * uint256 NFT price in wei
      */
-     //TODO Should this mapping be contained in a mapping (listingAddress => IERC721)?
-     //TODO Consider modularizing pricing strategies to support auctions, FOMO ramps, pricing tranches
+    //TODO Should this mapping be contained in a mapping (listingAddress => IERC721)?
+    //TODO Consider modularizing pricing strategies to support auctions, FOMO ramps, pricing tranches
     mapping(IERC721 => mapping(uint256 => uint256)) public prices;
 
     /**
@@ -81,11 +79,7 @@ contract NFTMarket is IERC721Receiver {
      * @param _contract The NFT's contract address.
      * @param _tokenId The NFT's tokenId.
      */
-    event Delisted(
-        address indexed _from,
-        IERC721 indexed _contract,
-        uint256 indexed _tokenId
-    );
+    event Delisted(address indexed _from, IERC721 indexed _contract, uint256 indexed _tokenId);
 
     /**
      * @notice Emitted when an NFT is purchased from NFTMKT.
@@ -123,14 +117,12 @@ contract NFTMarket is IERC721Receiver {
         IERC721 _contract,
         uint256 _tokenId,
         uint256 _price,
-        SaleRecipient[] memory _recipients // TODO @jango calldata?
-    ) external {
+        SaleRecipient[] memory _recipients
+    ) external nonReentrant {
+        // NFTMKT must be approved to manage this NFT or all NFTs from this contract
         require(
             _contract.getApproved(_tokenId) == address(this) ||
-                _contract.isApprovedForAll(
-                    _contract.ownerOf(_tokenId),
-                    address(this)
-                ), // TODO is this OR ok?
+                _contract.isApprovedForAll(_contract.ownerOf(_tokenId), address(this)),
             'NFTMKT::list: NOT_APPROVED'
         );
 
@@ -143,43 +135,30 @@ contract NFTMarket is IERC721Receiver {
         // Validate that recipients add up to no more than 100%.
         for (uint256 i = 0; i < _recipients.length; i++) {
             // The percent should be greater than 0.
-            require(
-                _recipients[i].percent > 0,
-                'NFTMKT::list: RECIPS_PERCENT_0'
-            );
+            require(_recipients[i].percent > 0, 'NFTMKT::list: RECIPS_PERCENT_0');
 
             // Add to the total percents.
-            saleRecipientsPercentTotal =
-                saleRecipientsPercentTotal +
-                _recipients[i].percent;
+            saleRecipientsPercentTotal = saleRecipientsPercentTotal + _recipients[i].percent;
 
             // The total percent should be less than 10000.
-            require(
-                saleRecipientsPercentTotal <= 10000,
-                'NFTMKT::list: RECIPS_PERCENT_EXCEEDS'
-            );
+            require(saleRecipientsPercentTotal <= 10000, 'NFTMKT::list: PERCENT_EXCEEDED');
 
             // If a projectId isn't specified, the beneficiary shouldn't be the zero address.
             // If a projectId is specified, a beneficiary of zero will send tokens to the purchaser.
             require(
-                _recipients[i].projectId != 0 ||
-                    _recipients[i].beneficiary != address(0),
+                _recipients[i].projectId != 0 || _recipients[i].beneficiary != address(0),
                 'NFTMKT::list: BENEF_IS_0.'
-
             );
-            // Set the recipients for this NFT listing to the passed `_recipients`.
+            // Add this recipient to the recipients list for this NFT listing.
             recipientsOf[msg.sender][_contract][_tokenId].push(_recipients[i]);
         }
 
         // If total sale recipients distribution is equal to 100%.
-        require(saleRecipientsPercentTotal == 10000, 'NFTMKT::list: RECIPS_PERCENT_TOTAL_NOT_100');
+        require(saleRecipientsPercentTotal == 10000, 'NFTMKT::list: PERCENT_NOT_100');
 
-        // Store the price
+        // Store the price //TODO Should store price as prices[listingAddress][_contract][_tokenId] ?
         prices[_contract][_tokenId] = _price;
 
-        // Transfer ownership of NFT to to the contract
-        // TODO @nicholas set this contract as the operator instead of transfering to this contract. right?
-        _contract.safeTransferFrom(msg.sender, address(this), _tokenId);
         emit Listed(msg.sender, _contract, _tokenId, _recipients, _price);
     }
 
@@ -190,7 +169,7 @@ contract NFTMarket is IERC721Receiver {
         IERC721 _contract,
         uint256 _tokenId,
         address _owner
-    ) external payable {
+    ) external payable nonReentrant{
         // TODO add reentrancy guard
         // must route funds received from buyer to the preconfigured recipients. Logic for this can be very similar to the _distributeToPayoutMods
         // see https://github.com/jbx-protocol/juicehouse/blob/540f3037689ae74f2f97d95f9f28d88f69afd4a3/packages/hardhat/contracts/TerminalV1.sol#L1015
@@ -199,9 +178,7 @@ contract NFTMarket is IERC721Receiver {
         require(prices[_contract][_tokenId] == msg.value, 'Incorrect ');
 
         // Get a reference to the sale recipients for this NFT.
-        SaleRecipient[] memory _recipients = recipientsOf[_owner][_contract][
-            _tokenId
-        ];
+        SaleRecipient[] memory _recipients = recipientsOf[_owner][_contract][_tokenId];
 
         // There must be recipients.
         require(_recipients.length > 0, 'Incorrect ');
@@ -215,32 +192,21 @@ contract NFTMarket is IERC721Receiver {
             SaleRecipient memory _recipient = _recipients[i];
 
             // The amount to send to recipients. Recipients percents are out of 10000.
-            uint256 _recipientCut = PRBMath.mulDiv(
-                msg.value,
-                _recipient.percent,
-                10000
-            );
+            uint256 _recipientCut = PRBMath.mulDiv(msg.value, _recipient.percent, 10000);
 
             // If the recipient is owed
             if (_recipientCut > 0) {
                 // And the recipient is a project
                 if (_recipient.projectId > 0) {
                     // Get a reference to the Juicebox terminal being used.
-                    ITerminal _terminal = terminalDirectory.terminalOf(
-                        _recipient.projectId
-                    );
+                    ITerminal _terminal = terminalDirectory.terminalOf(_recipient.projectId);
                     // If the project has a terminal
-                    require(
-                        _terminal != ITerminal(address(0)),
-                        'Terminal::pay: TERM_0'
-                    );
+                    require(_terminal != ITerminal(address(0)), 'Terminal::pay: TERM_0');
                     // Pay the terminal what this recipient is owed.
                     _terminal.pay{value: _recipientCut}(
                         _recipient.projectId,
                         // If no beneficiary is specified, send the tokens to the msg.sender.
-                        _recipient.beneficiary == address(0)
-                            ? msg.sender
-                            : _recipient.beneficiary,
+                        _recipient.beneficiary == address(0) ? msg.sender : _recipient.beneficiary,
                         _recipient.memo,
                         _recipient.preferUnstaked
                     );
@@ -257,32 +223,26 @@ contract NFTMarket is IERC721Receiver {
         // Delete the recipients.
         delete recipientsOf[_owner][_contract][_tokenId];
 
-        emit Purchased(
-            address(this),
-            msg.sender,
-            _contract,
-            _tokenId,
-            msg.value
-        );
+        emit Purchased(address(this), msg.sender, _contract, _tokenId, msg.value);
     }
 
     /**
      * @notice Cancels NFT listing on NFTMKT. Can only be called on an NFT by the address that listed it.
      */
-    function delist(IERC721 _contract, uint256 _tokenId) external {
+    function delist(IERC721 _contract, uint256 _tokenId) external nonReentrant {
         // Check that this contract is approved to move the NFT
         // TODO consider if we need this require. perhaps what matters more is that the calling address is approved, rather than the NFTMKT?
         require(
             _contract.getApproved(_tokenId) == address(this) ||
-                _contract.isApprovedForAll(
-                    _contract.ownerOf(_tokenId),
-                    address(this)
-                ),
+                _contract.isApprovedForAll(_contract.ownerOf(_tokenId), address(this)),
             'NFTMKT::delist: NOT_APPROVED'
         );
 
         // Check that caller listed the NFT
-        require(recipientsOf[msg.sender][_contract][_tokenId].length > 0, 'NFTMKT::delist: CALLER DID NOT LIST');
+        require(
+            recipientsOf[msg.sender][_contract][_tokenId].length > 0,
+            'NFTMKT::delist: CALLER DID NOT LIST'
+        );
 
         // Remove from recipientsOf
         delete recipientsOf[msg.sender][_contract][_tokenId];
