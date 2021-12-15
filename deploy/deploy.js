32c33fadc017fd25aa9344f0d3e52608bd7c69ed
nicholas <nnnnicholas@users.noreply.github.com>
2021-12-15 01:26:56 -0500

    deploy script formatting

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -11,9 +11,9 @@
 const rinkebyTerminalV1 = require(`@jbx-protocol/contracts/deployments/rinkeby/TerminalV1.json`);
 
 const getTerminalV1Address = (chainId) => {
-  if (chainId == 4) {
-    return rinkebyTerminalV1.address;
-  }
+    if (chainId == 4) {
+        return rinkebyTerminalV1.address;
+    }
 
     throw Error(`Chain ID ${chainId} is not yet supported.`);
 };
@@ -23,15 +23,15 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
     const { deployer } = await getNamedAccounts();
     const chainId = await getChainId();
 
-  const terminalV1Address = getTerminalV1Address(chainId);
-  console.log(
-    `Deploying Juice NFT Market with Chain ID {${chainId}} and TerminalV1 address ${terminalV1Address}`,
-  );
+    const terminalV1Address = getTerminalV1Address(chainId);
+    console.log(
+        `Deploying Juice NFT Market with Chain ID {${chainId}} and TerminalV1 address ${terminalV1Address}`,
+    );
 
-  await deploy('NFTMarket', {
-    from: deployer,
-    args: [terminalV1Address],
-    log: true,
-    skipIfAlreadyDeployed: true,
-  });
+    await deploy('NFTMarket', {
+        from: deployer,
+        args: [terminalV1Address],
+        log: true,
+        skipIfAlreadyDeployed: true,
+    });
 };
