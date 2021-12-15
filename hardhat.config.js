af3c35ae3ab39708fbb912ecaa180504011446c2
nicholas <nnnnicholas@users.noreply.github.com>
2021-12-15 00:28:27 -0500

    Added packages and updated .prettierrc

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,94 +14,94 @@ dotenv.config();
 const defaultNetwork = 'localhost';
 
 function mnemonic() {
-  try {
-    return fs.readFileSync('./mnemonic.txt').toString().trim();
-  } catch (e) {
-    if (defaultNetwork !== 'localhost') {
-      console.log(
-        '☢️ WARNING: No mnemonic file created for a deploy account. Try `yarn run generate` and then `yarn run account`.',
-      );
+    try {
+        return fs.readFileSync('./mnemonic.txt').toString().trim();
+    } catch (e) {
+        if (defaultNetwork !== 'localhost') {
+            console.log(
+                '☢️ WARNING: No mnemonic file created for a deploy account. Try `yarn run generate` and then `yarn run account`.',
+            );
+        }
     }
-  }
-  return '';
+    return '';
 }
 
 const infuraId = process.env.INFURA_ID;
 
 module.exports = {
-  defaultNetwork,
-  networks: {
-    localhost: {
-      url: 'http://localhost:8545',
+    defaultNetwork,
+    networks: {
+        localhost: {
+            url: 'http://localhost:8545',
+        },
+        rinkeby: {
+            url: 'https://rinkeby.infura.io/v3/' + infuraId,
+            accounts: {
+                mnemonic: mnemonic(),
+            },
+        },
+        mainnet: {
+            url: 'https://mainnet.infura.io/v3/' + infuraId,
+            gasPrice: 50000000000,
+            accounts: {
+                mnemonic: mnemonic(),
+            },
+        },
+        ropsten: {
+            url: 'https://ropsten.infura.io/v3/' + infuraId,
+            accounts: {
+                mnemonic: mnemonic(),
+            },
+        },
+        kovan: {
+            url: 'https://kovan.infura.io/v3/' + infuraId,
+            accounts: {
+                mnemonic: mnemonic(),
+            },
+        },
+        goerli: {
+            url: 'https://goerli.infura.io/v3/' + infuraId,
+            accounts: {
+                mnemonic: mnemonic(),
+            },
+        },
+        xdai: {
+            url: 'https://dai.poa.network',
+            gasPrice: 1000000000,
+            accounts: {
+                mnemonic: mnemonic(),
+            },
+        },
     },
-    rinkeby: {
-      url: 'https://rinkeby.infura.io/v3/' + infuraId,
-      accounts: {
-        mnemonic: mnemonic(),
-      },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        feeCollector: {
+            default: 0,
+        },
     },
-    mainnet: {
-      url: 'https://mainnet.infura.io/v3/' + infuraId,
-      gasPrice: 50000000000,
-      accounts: {
-        mnemonic: mnemonic(),
-      },
+    solidity: {
+        version: '0.8.6',
+        settings: {
+            optimizer: {
+                enabled: true,
+                // https://docs.soliditylang.org/en/v0.6.3/using-the-compiler.html
+                runs: 10000,
+            },
+        },
     },
-    ropsten: {
-      url: 'https://ropsten.infura.io/v3/' + infuraId,
-      accounts: {
-        mnemonic: mnemonic(),
-      },
+    mocha: {
+        bail: true,
+        timeout: 6000,
     },
-    kovan: {
-      url: 'https://kovan.infura.io/v3/' + infuraId,
-      accounts: {
-        mnemonic: mnemonic(),
-      },
+    gasReporter: {
+        currency: 'USD',
+        // gasPrice: 21,
+        enabled: !!process.env.REPORT_GAS,
+        showTimeSpent: true,
     },
-    goerli: {
-      url: 'https://goerli.infura.io/v3/' + infuraId,
-      accounts: {
-        mnemonic: mnemonic(),
-      },
+    etherscan: {
+        apiKey: `${process.env.ETHERSCAN_API_KEY}`,
     },
-    xdai: {
-      url: 'https://dai.poa.network',
-      gasPrice: 1000000000,
-      accounts: {
-        mnemonic: mnemonic(),
-      },
-    },
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-    },
-    feeCollector: {
-      default: 0,
-    },
-  },
-  solidity: {
-    version: '0.8.6',
-    settings: {
-      optimizer: {
-        enabled: true,
-        // https://docs.soliditylang.org/en/v0.6.3/using-the-compiler.html
-        runs: 10000,
-      },
-    },
-  },
-  mocha: {
-    bail: true,
-    timeout: 6000,
-  },
-  gasReporter: {
-    currency: 'USD',
-    // gasPrice: 21,
-    enabled: !!process.env.REPORT_GAS,
-    showTimeSpent: true,
-  },
-  etherscan: {
-    apiKey: `${process.env.ETHERSCAN_API_KEY}`,
-  },
 };
