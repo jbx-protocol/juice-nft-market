7628ff0cbe9ffbfe8f1ec573f67332853be6dcb9
nicholas <nnnnicholas@users.noreply.github.com>
2022-01-06 10:43:25 +0100

    added nft minting to list test setup()

--- test/list.test.js
+++ test/list.test.js
@@ -6,21 +6,28 @@ import rinkebyTerminalV1 from '@jbx-protocol/contracts/deployments/rinkeby/Termi
 
 describe('List', () => {
     async function setup() {
+        //
         const [deployer, nftDeployer] = await ethers.getSigners();
-        const mockTerminalV1 = await deployMockContract(deployer, rinkebyTerminalV1.abi);
         
+        // Deploy nftMarket
+        const mockTerminalV1 = await deployMockContract(deployer, rinkebyTerminalV1.abi);
         const nftMarketFactory = await ethers.getContractFactory('NFTMarket');
         const nftMarket = await nftMarketFactory.deploy(mockTerminalV1.address);
         
+        // Deploy NFT
         const nftFactory = await ethers.getContractFactory('ERC721');
         const nft = await nftFactory.deploy('Name', 'NAME');
-        
+
+        // Mint NFT from nftDeployer to nftDeployer
+        await nft.safeMint(nftDeployer.address, 1);
+        await nft.connect(deployer).safeMint(deployer.address, 2);
+
         return { deployer, nftDeployer, mockTerminalV1, nftMarket, nft };
     }
 
-    it('should deploy the NFTMarket contract', async () => {
+    it('Should fail if token allowance is not set.', async () => {
         const { deployer, nftDeployer, mockTerminalV1, nftMarket, nft } = await setup();
-        await nft.safeMint(nftDeployer.address, 1);
-        console.log(await nft.balanceOf(nftDeployer.address));
+
+
     });
 });
