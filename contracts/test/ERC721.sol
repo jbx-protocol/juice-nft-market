0906c6cf1e498fb50bb09ffac35b3a75a4d85903
nicholas <nnnnicholas@users.noreply.github.com>
2022-01-06 10:42:57 +0100

    added Ownable to 721

--- contracts/test/ERC721.sol
+++ contracts/test/ERC721.sol
@@ -10,13 +10,14 @@ import '@openzeppelin/contracts/utils/Address.sol';
 import '@openzeppelin/contracts/utils/Context.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
 import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
 /**
  * @dev Implementation of https://eips.ethereum.org/EIPS/eip-721[ERC721] Non-Fungible Token Standard, including
  * the Metadata extension, but not including the Enumerable extension, which is available separately as
  * {ERC721Enumerable}.
  */
-contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
+contract ERC721 is Context, ERC165, IERC721, IERC721Metadata, Ownable {
     using Address for address;
     using Strings for uint256;
 
@@ -479,7 +480,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata {
 
     // CUSTOM FUNCTIONS // 
 
-    function safeMint(address to, uint256 tokenId) external {
+    function safeMint(address to, uint256 tokenId) external onlyOwner {
         _safeMint(to, tokenId);
     }
 
