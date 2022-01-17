5c68f7eef6f2da4e0a4a37b5c5e30ac4220176cd
nicholas <nnnnicholas@users.noreply.github.com>
2022-01-16 23:17:06 -0500

    fixed tests

--- test/list.test.js
+++ test/list.test.js
@@ -37,26 +37,34 @@ describe('List', () => {
         ]
     };
 
-    it('Should succeed if listed by owner', async () => {
+    it('Should succeed if listed by owner and Market IS authorized', async () => {
         const { deployer, nftDeployer, nftMarket, nft } = await setup();
+        await nft.connect(nftDeployer).approve(nftMarket.address, 1);
         await expect(nftMarket.connect(nftDeployer).list(nft.address, 1, 1, makeSaleReceipientArray(10000, deployer.address, 1))).to.not.be.reverted;
-    })
+    }); 
 
-    it('Should succeed if listed by address approved for token', async () => {
+    it('Should revert if listed by owner and Market is NOT authorized.', async () => {
         const { deployer, nftDeployer, nftMarket, nft } = await setup();
-        await nft.connect(nftDeployer).approve(nftMarket.address, 1); // approve nftMarket
-        await expect(nftMarket.list(nft.address, 1, 1, makeSaleReceipientArray(10000, deployer.address, 1))).to.not.be.reverted;
+        // console.log(await nft.getApproved(1))
+        // console.log(await nft.isApprovedForAll(nftDeployer.address, deployer.address));
+        await expect(nftMarket.connect(nftDeployer).list(nft.address, 1, 1, makeSaleReceipientArray(10000, deployer.address, 1))).to.be.revertedWith('Unapproved()');
     })
 
-    it('Should succeed if listed by address approved for all tokens', async () => {
-        const { deployer, nftDeployer, nftMarket, nft } = await setup();
-        await nft.connect(nftDeployer).setApprovalForAll(nftMarket.address, 1); // approve nftMarket
-        await expect(nftMarket.connect(nftDeployer).list(nft.address, 1, 1, makeSaleReceipientArray(10000, deployer.address, 1))).to.not.be.reverted;
-    })
-
-    // it('Should fail if token does not belong to listing address', async () => {
+    // it('Should succeed if listed by address approved for token', async () => {
     //     const { deployer, nftDeployer, nftMarket, nft } = await setup();
-    //     // expect(await nftMarket.list(nft.address, 1, 1, makeSaleReceipientArray(10000, deployer.address, 1))).to.be.revertedWith('Unapproved()');
+    //     await nft.connect(nftDeployer).approve(nftMarket.address, 1); // approve nftMarket
+    //     await expect(nftMarket.list(nft.address, 1, 1, makeSaleReceipientArray(10000, deployer.address, 1))).to.not.be.reverted;
+    // })
+
+    // it('Should succeed if listed by address approved for all tokens', async () => {
+    //     const { deployer, nftDeployer, nftMarket, nft } = await setup();
+    //     await nft.connect(nftDeployer).setApprovalForAll(nftMarket.address, 1); // approve nftMarket
+    //     await expect(nftMarket.connect(nftDeployer).list(nft.address, 1, 1, makeSaleReceipientArray(10000, deployer.address, 1))).to.not.be.reverted;
+    // })
+
+    // it('Should fail if token does not belong to listing address and is is not app', async () => {
+    //     const { deployer, nftDeployer, nftMarket, nft } = await setup();
+        // expect(await nftMarket.list(nft.address, 1, 1, makeSaleReceipientArray(10000, deployer.address, 1))).to.be.revertedWith('Unapproved()');
     // });
 
     // it('Should fail if token allowance is not set.', async () => {
